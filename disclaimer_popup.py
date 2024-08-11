2fb0af359335ac1ec86c29ab2b2d19ea101a8496
gustavomarcelonunez <gnunez@ing.unp.edu.ar>
2024-08-11 20:58:30 -0300

    Boton de disclaimer

--- disclaimer_popup.py
+++ disclaimer_popup.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
-# Función para mostrar el mensaje de advertencia
+
+@st.dialog("Cast your vote")
 def show_disclaimer_popup():
-    st.toast("Disclaimer: This chat application, 🤖 GBIF EcoQuery, was developed for the GBIF Ebbe Nielsen Challenge 2024 and is non-commercial in nature. It uses data retrieved from the GBIF REST API, limited to 9 datasets and the first 20 occurrences per dataset, due to processing constraints of the utilized Large Language Model (LLM). Please note that while we strive for accuracy, the use of LLMs may result in occasional inaccuracies or incomplete information. Users are encouraged to verify critical information independently.")
+
+    st.write("Algo adentro?")
+
+   # st.dialog("Disclaimer: This chat application, 🤖 GBIF EcoQuery, was developed for the GBIF Ebbe Nielsen Challenge 2024 and is non-commercial in nature. It uses data retrieved from the GBIF REST API, limited to 9 datasets and the first 20 occurrences per dataset, due to processing constraints of the utilized Large Language Model (LLM). Please note that while we strive for accuracy, the use of LLMs may result in occasional inaccuracies or incomplete information. Users are encouraged to verify critical information independently.")
+    if st.button("Close"):
+         st.rerun()
