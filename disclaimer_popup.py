f2504ef580a3cad370d16405bc06ba01da53304c
gustavomarcelonunez <gnunez@ing.unp.edu.ar>
2024-08-11 19:25:40 -0300

    Boton de disclaimer

--- disclaimer_popup.py
+++ disclaimer_popup.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 # Función para mostrar el mensaje de advertencia
 def show_disclaimer_popup():
-    with st.popover("Open popover"):
+    with st.sidebar.button(st.popover("Open popover")):
         st.markdown("Hello World 👋")
