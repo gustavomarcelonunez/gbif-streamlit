8d04fd94a3a361de6864484a4cd4d0de102fa3e2
gustavomarcelonunez <gnunez@ing.unp.edu.ar>
2024-08-11 19:09:14 -0300

    Boton de disclaimer

--- disclaimer_popup.py
+++ disclaimer_popup.py
@@ -4,9 +4,9 @@ def show_disclaimer():
     st.markdown("### Disclaimer")
     st.write(
         """
-        This chat application, 🤖 GBIF EcoQuery Bot, was developed for the GBIF Ebbe Nielsen Challenge 2024 and is non-commercial in nature.
+        This chat application, 🤖 GBIBot, was developed for the GBIF Ebbe Nielsen Challenge 2024 and is non-commercial in nature.
         
-        It uses data retrieved from the GBIF REST API, limited to 9 datasets and the first 20 occurrences per dataset, due to processing constraints of the utilized Large Language Model (LLM).
+        It uses data retrieved from the GBIF REST API, limited to 10 datasets and the first 20 occurrences per dataset, due to processing constraints of the utilized Large Language Model (LLM).
         
         Please note that while we strive for accuracy, the use of LLMs may result in occasional inaccuracies or incomplete information. Users are encouraged to verify critical information independently.
         """
@@ -17,11 +17,17 @@ def show_disclaimer_sidebar():
         st.session_state.show_disclaimer = False
 
     with st.sidebar:
-        if st.button("Disclaimer"):
+        if st.button("Show Disclaimer"):
             st.session_state.show_disclaimer = True
 
     if st.session_state.show_disclaimer:
         show_disclaimer()
         if st.button("Close Disclaimer"):
             st.session_state.show_disclaimer = False
-            st.experimental_rerun()  # Fuerza la actualización de la página
+            # Recarga el mismo código sin recargar la página
+            st.experimental_set_query_params(disclaimer="closed")
+
+# Al cargar la app, revisa si el parámetro "disclaimer" está presente en la URL
+query_params = st.experimental_get_query_params()
+if query_params.get("disclaimer") == ["closed"]:
+    st.session_state.show_disclaimer = False
