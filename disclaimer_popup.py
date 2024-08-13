f87162eb87220505afbc7eb95894634a4a2bc78c
gustavomarcelonunez <gnunez@ing.unp.edu.ar>
2024-08-13 10:03:23 -0300

    Modificaciones de codigo

--- disclaimer_popup.py
+++ disclaimer_popup.py
@@ -5,7 +5,7 @@ def show_disclaimer_popup():
 
     st.write(
         """
-        This chat application, 🤖 BotGBIF, was developed for the [GBIF Ebbe Nielsen Challenge 2024](https://www.gbif.org/es/ebbe) and is non-commercial in nature. It uses data retrieved from the [GBIF REST API](https://techdocs.gbif.org/en/openapi/), limited to 9 datasets and the first 100 occurrences per dataset, due to processing constraints of the utilized Large Language Model [(LLM)](https://platform.openai.com/settings/organization/limits). Please note that while we strive for accuracy, the use of LLMs may result in occasional inaccuracies or incomplete information. Users are encouraged to verify critical information independently.
+        This chat application, 🤖 BotGBIF, was developed for the [GBIF Ebbe Nielsen Challenge 2024](https://www.gbif.org/es/ebbe) and is non-commercial in nature. It uses data retrieved from the [GBIF REST API](https://techdocs.gbif.org/en/openapi/), limited to 9 datasets and the first 100 occurrences per dataset, due to processing constraints of the utilized Large Language Model [(LLM)](https://platform.openai.com/settings/organization/limits). Please note that while we strive for accuracy, the use of LLMs may result in occasional inaccuracies or incomplete information. Users are encouraged to verify critical information independently. More information about the repository [here](https://github.com/gustavomarcelonunez/gbif-streamlit/tree/devGus).
         """
     )
     if st.button("Close"):
